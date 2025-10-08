c4ce2150a8a4e9426d34124cc3603d555c29cc18
duyh80456-code <duyh80456@gmail.com>
2025-10-09 02:41:51 +0700

    Add files via upload
    upload file

---
+++ src/main/java/vnu/uet/goldexperience/SceneManager.java
@@ -0,0 +1,58 @@
+package vnu.uet.goldexperience;
+
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+import javafx.scene.Node;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class SceneManager {
+    private final StackPane root;
+    protected final Map<String, FXMLLoader> screens;
+
+    public SceneManager(StackPane root) {
+        this.root = root;
+        screens = new HashMap<>();
+    }
+
+    public void PreloadScene(String name, String fxmlPath) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+        loader.load();
+        screens.put(name, loader);
+    }
+
+    public void switchTo(String name){
+        FXMLLoader loader = screens.get(name);
+        if (loader == null) {
+            System.err.println("Scene not found: " + name);
+            return;
+        }
+
+        Parent newScreen = loader.getRoot();
+        if (root.getChildren().isEmpty()){
+            root.getChildren().add(newScreen);
+            return;
+        }
+
+        Node current = root.getChildren().get(0);
+
+        FadeTransition fadeOut = new FadeTransition(Duration.millis(400), current);
+        fadeOut.setToValue(0);
+
+        fadeOut.setOnFinished(e -> {
+            root.getChildren().setAll(newScreen);
+            newScreen.setOpacity(0);
+
+            FadeTransition fadeIn = new FadeTransition(Duration.millis(400), newScreen);
+            fadeIn.setToValue(1);
+            fadeIn.play();
+        });
+        fadeOut.play();
+    }
+}
