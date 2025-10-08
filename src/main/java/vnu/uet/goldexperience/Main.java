c7600d71047e5e4ecb565fd0962939e3066cba9f
duyh80456-code <duyh80456@gmail.com>
2025-10-09 02:46:30 +0700

    Update Main.java

--- src/main/java/vnu/uet/goldexperience/Main.java
+++ src/main/java/vnu/uet/goldexperience/Main.java
@@ -2,20 +2,38 @@ package vnu.uet.goldexperience;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
+
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/menu.fxml"));
-        Scene scene = new Scene(root);
-        stage.setTitle("Game Menu");
+        StackPane root = new StackPane();
+        SceneManager sceneManager = new SceneManager(root);
+
+        // preload FXML
+        sceneManager.PreloadScene("menu","/vnu/uet/goldexperience/menu-view.fxml");
+        sceneManager.PreloadScene("tutorial", "/vnu/uet/goldexperience/tutorial-view.fxml");
+
+        //get signal from menu screen
+        FXMLLoader menuLoader = sceneManager.screens.get("menu");
+        MenuController menuController = menuLoader.getController();
+        menuController.setSceneManager(sceneManager);
+
+        //get signal from tutorial screen
+        FXMLLoader tutorialLoader = sceneManager.screens.get("tutorial");
+        MenuController tutorialController = tutorialLoader.getController();
+        tutorialController.setSceneManager(sceneManager);
+
+        sceneManager.switchTo("menu");
+
+        Scene scene = new Scene(root,1280,720);
+        scene.getStylesheets().add(getClass().getResource("/vnu/uet/goldexperience/style.css").toExternalForm());
+
         stage.setScene(scene);
         stage.show();
-
     }
 
     public static void main(String[] args) {
